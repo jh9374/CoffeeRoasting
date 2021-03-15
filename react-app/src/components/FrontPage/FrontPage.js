1c9aedb4d4d4669bb34a3ce8ef0a318ed57622bc
jh9374 <39279427+jh9374@users.noreply.github.com>
2021-03-15 00:04:21 -0500

    added search bar

--- react-app/src/components/FrontPage/FrontPage.js
+++ react-app/src/components/FrontPage/FrontPage.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 // Styling
 import "./FrontPage.css"
@@ -7,24 +8,54 @@ import "./FrontPage.css"
 import toast_color from "../../images/toast_color.png";
 import coffeeRoasting from "../../images/coffeeRoasting.png"
 
-function FrontPage(){
+function FrontPage() {
+
+    const [search, setSearch] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+    }
     return (
         <>
-        <div className="frontpage">
-            <div className="frontpage__banner">
-                <div className="frontpage__text">
-                    Ready to "Roast" or "Toast"?
+            <div className="frontpage">
+                <div className="frontpage__banner">
+                    <div className="frontpage__text">
+                        Ready to "Roast" or "Toast"?
+                </div>
+                <div className="search__wrapper">
+                    <form className="search__form" onSubmit={handleSubmit}>
+                        <div className="search-text__wrapper">
+                            <h2 className="search__text">
+                                Search for Coffee
+                            </h2>
+                        </div>
+                        <div className="input__wrapper">
+                            <input
+                                value={search}
+                                placeholder="Counter Culture"
+                                onChange={(e) => setSearch(e.target.value)}
+                                className="search__input">
+                            </input>
+                        </div>
+                        <div className="submit__wrapper">
+                            <button
+                                type="submit"
+                                className="search__button">
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                </div>
+                <div className="frontpage__images">
+                    <img className="roast__image" src={coffeeRoasting} alt="coffee roasting" />
+                    <img className="toast__image" src={toast_color} alt="toast" />
                 </div>
             </div>
-            <div className="frontpage__images">
-                <img className="roast__image" src={coffeeRoasting} alt="coffee roasting" />
-                <img className="toast__image" src={toast_color} alt="toast" />
-            </div>
-        </div>
-        <footer>
-            <div>Github</div>
-            <div>LinkedIn</div>
-        </footer>
+            <footer>
+                <div>Github</div>
+                <div>LinkedIn</div>
+            </footer>
         </>
     )
 }
