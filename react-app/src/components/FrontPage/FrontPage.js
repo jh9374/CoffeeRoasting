20748def35a6dc8b91b78b1e9fc0878b27e8ce6b
jh9374 <39279427+jh9374@users.noreply.github.com>
2021-03-14 23:33:26 -0500

    styled front page

--- react-app/src/components/FrontPage/FrontPage.js
+++ react-app/src/components/FrontPage/FrontPage.js
@@ -3,15 +3,29 @@ import React from "react";
 // Styling
 import "./FrontPage.css"
 
+// Images
+import toast_color from "../../images/toast_color.png";
+import coffeeRoasting from "../../images/coffeeRoasting.png"
+
 function FrontPage(){
     return (
+        <>
         <div className="frontpage">
             <div className="frontpage__banner">
                 <div className="frontpage__text">
-                    Welcome to Coffee Roasting,<br></br> where you can "Roast" or "Toast" a Coffee Roast
+                    Ready to "Roast" or "Toast"?
                 </div>
             </div>
+            <div className="frontpage__images">
+                <img className="roast__image" src={coffeeRoasting} alt="coffee roasting" />
+                <img className="toast__image" src={toast_color} alt="toast" />
+            </div>
         </div>
+        <footer>
+            <div>Github</div>
+            <div>LinkedIn</div>
+        </footer>
+        </>
     )
 }
 
